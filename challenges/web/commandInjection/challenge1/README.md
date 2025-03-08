17ff5ead3067f758ec916809f111563df8a67a2a
Ely Sylvain Schybol <ely.schybol@stud.htwk-leipzig.de>
2025-03-08 21:01:11 +0100

    <feat> breaked up the challenge in two seperate docker container && fixed all the things mentioned in the review

--- challenges/web/commandInjection/challenge1/README.md
+++ challenges/web/commandInjection/challenge1/README.md
@@ -12,3 +12,7 @@ sudo docker build -t commandinjectionlab .
 ```bash
 sudo docker run --rm -it -p 8080:5000 -v ./.env:/commandInjectionLab/.env --name commandinjectionlab-container commandinjectionlab
 ```
+-> The application is noew available at `http://localhost:8080/`
+
+# Challenge
+Try to escalate the command injection vulnerabilities and read `flag.txt` at the source folder. Please don't break stuff. Thank you!
--- challenges/web/commandInjection/challenge1/main.py
+++ challenges/web/commandInjection/challenge1/main.py
@@ -17,18 +17,5 @@ def firstEncounter():
 				return render_template('firstEncounter.html', args=baddie)
 	return render_template('firstEncounter.html', args="Beautiful Content")
 
-# sanitization -> commands
-@app.route('/secondEncounter', methods=['GET'])
-def secondEncounter():
-	if request.method == 'GET':
-			if request.args.get('param') != None:
-				oupsi = request.args.get('param')
-				if any(i in oupsi for i in ['whoami', 'cat', 'ls']):
-					return render_template('secondEncounter.html', args="Please don't hack this site")
-				baddie = (os.popen(f'ping -c 1 {oupsi}').read())
-				return render_template('secondEncounter.html', args=baddie)
-	return render_template('secondEncounter.html', args="Beautiful Content")
-
-
 if __name__ == '__main__':
 	app.run()
